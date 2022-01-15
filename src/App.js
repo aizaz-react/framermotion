4b244aa5b5a237da226b5fadfebbe00d1da12643
aizaz-react <aizaz9790@gmail.com>
2022-01-16 01:15:07 +0500

    framer motion

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { AnimatePresence } from "framer-motion";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import BackgroundImage from "./assets/background-image.svg";
+
+import WelcomePage from "./containers/WelcomePage";
+import LoginPage from "./containers/LoginPage";
+import CreatePasswordPage from "./containers/CreatePasswordPage";
+import HomePage from "./containers/HomePage";
+
+import Rules from "./containers/LogicalPuzzle/Rules";
+import LogicalPuzzle from "./containers/LogicalPuzzle/LogicalPuzzle";
+import MazePage from "./containers/mazePage";
+function App() {
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <div className="App">
+        <Router>
+          <div className="app_wrap">
+            <div>
+              <img
+                className="app_bg_img"
+                src={BackgroundImage}
+                alt="background-img"
+              />
+            </div>
+
+            <div className="app_content">
+              <div className="content">
+                <div className="pages">
+                  <Routes>
+                    <Route path="/welcome" element={<WelcomePage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route
+                      path="/create-password"
+                      element={<CreatePasswordPage />}
+                    />
+                    {/* MazePage */}
+                    {/* <Route path="/" element={<HomePage />} /> */}
+                    <Route path="/" element={<MazePage />} />
+                    <Route path="/logical-puzzle/rules" element={<Rules />} />
+                    <Route path="/logical-puzzle" element={<LogicalPuzzle />} />
+                  </Routes>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Router>
+      </div>
+    </AnimatePresence>
+  );
+}
+
+export default App;
---
+++ src/containers/mazePage.js
@@ -0,0 +1,205 @@
+import React from "react";
+import { motion } from "framer-motion";
+import donut from "../assets/balls/Ball2.svg";
+import ball from "../assets/balls/Ball1.svg";
+
+const MazePage = () => {
+  const mainText = {
+    hidden: {
+      y: 0,
+    },
+    visible: {
+      textShadow: ["", "", "5px 1px 19px rgb(247, 145, 247)"],
+      transition: {
+        delay: 1.5,
+        duration: 5,
+      },
+    },
+  };
+
+  const mainText1 = {
+    hidden: {
+      y: 0,
+    },
+    visible: {
+      textShadow: ["", "", "5px 1px 19px rgb(247, 145, 247)"],
+      transition: {
+        delay: 2.5,
+        duration: 3,
+      },
+    },
+  };
+
+  const followText = {
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: 3,
+        duration: 1,
+      },
+    },
+  };
+
+  const ballimages = {
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: 3.5,
+        duration: 1,
+      },
+    },
+  };
+
+  const donutimages = {
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: 6,
+        duration: 2,
+      },
+    },
+  };
+
+  const firstRule = {
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: 5.5,
+        duration: 1,
+      },
+    },
+  };
+
+  const secondRule = {
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: 8,
+        duration: 1,
+      },
+    },
+  };
+
+  const button = {
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        // type: "spring",
+        // stiffness: 70,
+        delay: 8,
+        // duration: 3,
+      },
+    },
+  };
+
+  return (
+    <div className="maze_page">
+      <p className="main-text">
+        Complete the task{" "}
+        <motion.span variants={mainText} initial="hidden" animate="visible">
+          correctly
+        </motion.span>{" "}
+        and{" "}
+        <motion.span variants={mainText1} initial="hidden" animate="visible">
+          quickly
+        </motion.span>{" "}
+        as possible
+      </p>
+      <motion.p
+        variants={followText}
+        initial={{ opacity: 0 }}
+        animate={followText.visible}
+        className="main-text"
+      >
+        FOLLOW THE RULES
+      </motion.p>
+      <div className="donut-section">
+        <motion.img
+          variants={ballimages}
+          initial={{ x: -100, opacity: 0 }}
+          animate={ballimages.visible}
+          src={ball}
+          alt={ball}
+        />
+        <motion.p
+          variants={ballimages}
+          initial={{ x: -150, opacity: 0 }}
+          animate={ballimages.visible}
+        >
+          - - - - - - - - -
+        </motion.p>
+        <motion.p
+          variants={firstRule}
+          initial={{ opacity: 0 }}
+          animate={firstRule.visible}
+        >
+          Can never be close to
+        </motion.p>
+        <motion.p
+          variants={ballimages}
+          initial={{ x: 150, opacity: 0 }}
+          animate={ballimages.visible}
+        >
+          - - - - - - - - -
+        </motion.p>
+        <motion.img
+          variants={ballimages}
+          initial={{ x: 100, opacity: 0 }}
+          animate={ballimages.visible}
+          src={ball}
+          alt={ball}
+        />
+      </div>
+      <div className="donut-section">
+        <motion.img
+          variants={donutimages}
+          initial={{ x: 700, opacity: 0 }}
+          src={donut}
+          animate={donutimages.visible}
+          alt={donut}
+        />
+        <motion.p
+          variants={donutimages}
+          initial={{ x: 600, opacity: 0 }}
+          animate={donutimages.visible}
+        >
+          - - - - - - - - -
+        </motion.p>
+        <motion.p
+          variants={secondRule}
+          initial={{ opacity: 0 }}
+          animate={secondRule.visible}
+        >
+          Cannot be at the left end
+        </motion.p>
+        <motion.p
+          variants={donutimages}
+          initial={{ x: -600, opacity: 0 }}
+          animate={donutimages.visible}
+        >
+          - - - - - - - - -
+        </motion.p>
+        <motion.img
+          variants={donutimages}
+          initial={{ x: -700, opacity: 0 }}
+          animate={donutimages.visible}
+          src={donut}
+          alt={donut}
+        />
+      </div>
+
+      <motion.button
+        variants={button}
+        initial={{ y: 80, opacity: 0 }}
+        animate={button.visible}
+      >
+        OK, LET'S START
+      </motion.button>
+    </div>
+  );
+};
+
+export default MazePage;
